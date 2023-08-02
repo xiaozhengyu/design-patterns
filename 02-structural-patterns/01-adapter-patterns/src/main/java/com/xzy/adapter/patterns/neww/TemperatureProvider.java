80d63452f41ed61337ac32831ba63e59d1a8a139
zhengyuxiao <zhengyupython@qq.com>
2023-08-02 22:29:18 +0800

    适配器模式

---
+++ 02-structural-patterns/01-adapter-patterns/src/main/java/com/xzy/adapter/patterns/neww/TemperatureProvider.java
@@ -0,0 +1,16 @@
+package com.xzy.adapter.patterns.neww;
+
+/**
+ * 新的接口
+ *
+ * @author xzy
+ * @date 2023/8/2 22:04
+ */
+public interface TemperatureProvider {
+    /**
+     * 返回以摄氏度为单位的温度
+     *
+     * @return -
+     */
+    double getTemperatureInCelsius();
+}
