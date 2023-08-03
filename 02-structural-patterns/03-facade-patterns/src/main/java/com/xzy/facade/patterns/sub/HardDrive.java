5fac9c90a72e70b2f9e92c7eb2d0516218a7591c
xiaozhegnyu <1415841836@qq.com>
2023-08-03 20:55:54 +0800

    外观模式

---
+++ 02-structural-patterns/03-facade-patterns/src/main/java/com/xzy/facade/patterns/sub/HardDrive.java
@@ -0,0 +1,13 @@
+package com.xzy.facade.patterns.sub;
+
+/**
+ * 子系统
+ *
+ * @author xzy.xiao
+ * @date 2023/8/3  20:52
+ */
+public class HardDrive {
+    public void init() {
+        System.out.println("HardDrive initialized");
+    }
+}
